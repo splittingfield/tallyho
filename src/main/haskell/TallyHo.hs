135a61e6107564671e468dfad640c143c8ef9bc6
Robert J. Macomber <robert.macomber@socrata.com>
2014-11-13 12:53:05 -0800

    Rearrange things
    * TallyHo.All => TallyHo
    * Change constraints on demo conduit
      - use MonadBaseControl instead of MonadTrans
      - this allows nesting of arbitrary depth
    * Correct the cabal file

---
+++ src/main/haskell/TallyHo.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
+
+module TallyHo (
+  module TallyHo.HyperLogLogC
+, module TallyHo.CountMinSketchC
+, wordCounter, Result(..)
+) where
+
+import ClassyPrelude.Conduit
+import Data.Text (splitOn)
+
+import TallyHo.HyperLogLogC
+import TallyHo.CountMinSketchC
+
+data Result = Result { cms :: CountMinSketch Text
+                     , card :: Int64 }
+
+-- | Read UTF-8 encoded text from a bytestream source, and count both
+-- individual and distinct words.
+wordCounter :: (MonadThrow m, MonadBaseControl b m, PrimMonad b) => Sink ByteString m Result
+wordCounter =
+  decodeUtf8C $= linesUnboundedC
+              $= awaitForever (mapM_ yield . splitOn " ")
+              $= filterC (/= "")
+              $= (getZipSink $
+                    Result <$> (ZipSink $ transPipe liftBase $ countMinSketchC 20 20)
+                           <*> (ZipSink $ mapC unpack $= hyperLogLogC))
