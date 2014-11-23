34597055676718d61de3b4bcb99200175b33fc91
Robert J. Macomber <robert.macomber@socrata.com>
2014-11-22 17:10:11 -0800

    HyperLogLog issue #8 fixed

--- src/main/haskell/TallyHo/HyperLogLogC.hs
+++ src/main/haskell/TallyHo/HyperLogLogC.hs
@@ -7,11 +7,10 @@ module TallyHo.HyperLogLogC (
 import ClassyPrelude.Conduit
 import Control.Lens
 import Data.Approximate (estimate)
-import Data.Digest.Murmur32 (Hashable32, asWord32, hash32)
 import Data.HyperLogLog (insert, size, HyperLogLog)
+import Data.Bytes.Serial (Serial)
 
 type HLL = HyperLogLog $(12)
 
--- Hashable32 and preliminary mapC because of https://github.com/ekmett/hyperloglog/issues/8
-hyperLogLogC :: (Monad m, Hashable32 a) => Sink a m Int64
-hyperLogLogC = mapC (asWord32 . hash32) $= foldlC (flip insert) (mempty :: HLL) <&> size <&> (^.estimate)
+hyperLogLogC :: (Monad m, Serial a) => Sink a m Int64
+hyperLogLogC = foldlC (flip insert) (mempty :: HLL) <&> size <&> (^.estimate)
